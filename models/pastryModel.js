c4260102723cc16395a5ff7ee455b99ed062f6a7
OctoDev8 <gabrielmejorada12@gmail.com>
2024-11-22 08:29:54 -0700

    Added data schemas

--- models/pastryModel.js
+++ models/pastryModel.js
@@ -1,1 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
+const pastrySchema = new mongoose.Schema({
+	pastryType: { type: String, required: true },
+	description: { type: String, required: true },
+	price: { type: Number, required: true },
+	weight: { type: Number, required: true }, // in grams
+	ingredients: [{ type: String, required: true }], // Array of ingredients
+	inventory: { type: Number, required: true }, // Quantity in stock
+});
+
+const Pastry = mongoose.model("Pastry", pastrySchema);
+
+module.exports = Pastry;
