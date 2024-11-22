94409cac59362b5768100596632d9d77505dad5f
OctoDev8 <gabrielmejorada12@gmail.com>
2024-11-22 08:48:26 -0700

    Syncing old data schemas to my fork

--- models/breadModel.js
+++ models/breadModel.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 // Bread Schema
 
 const breadSchema = new mongoose.Schema({
-  breadType: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  weight: { type: Number, required: true }, // in grams
-  shelfLife: { type: String, required: true }, // e.g., "3 days"
-  ingredients: [{ type: String, required: true }], // Array of ingredients
-  inventory: { type: Number, required: true }, // Quantity in stock
+	breadType: { type: String, required: true },
+	description: { type: String, required: true },
+	price: { type: Number, required: true },
+	weight: { type: Number, required: true }, // in grams
+	shelfLife: { type: String, required: true }, // e.g., "3 days"
+	ingredients: [{ type: String, required: true }], // Array of ingredients
+	inventory: { type: Number, required: true }, // Quantity in stock
 });
 
 module.exports = mongoose.model("Bread", breadSchema);
