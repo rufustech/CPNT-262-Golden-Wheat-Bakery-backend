87670fd9583633af1830dee5309639ab1a793cbe
OctoDev8 <gabrielmejorada12@gmail.com>
2024-11-22 08:37:42 -0700

    Pushing new data schemas

--- models/bunsModel.js
+++ models/bunsModel.js
@@ -1,1 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
+const bunSchema = new mongoose.Schema({
+	bunType: { type: String, required: true },
+	description: { type: String, required: true },
+	price: { type: Number, required: true },
+	weight: { type: Number, required: true }, // in grams
+	ingredients: [{ type: String, required: true }], // Array of ingredients
+	inventory: { type: Number, required: true }, // Quantity in stock
+});
+
+const Bun = mongoose.model("Bun", bunSchema);
+
+module.exports = Bun;
