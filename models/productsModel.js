5ecba0300a5095ef85df273b66c96b4f2fdc8d5a
rmucheri <vamucheri@gmail.com>
2024-11-28 14:03:34 -0700

    order fix, productSchemaFix

--- models/productsModel.js
+++ models/productsModel.js
@@ -6,9 +6,11 @@ const productSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   weight: { type: Number },
   shelfLife: { type: String },
+  image: { type: String },
   ingredients: [{ type: String }],
   inventory: { type: Number, required: true, default: 0 },
-  category: { type: String, required: true }, // e.g., 'Bread', 'Buns', 'Pastries'
+  category: { type: String, required: true }, // e.g., 'Bread', 'Buns', 'Pastries',
+  image: { type: String },
 });
 
 module.exports = mongoose.model("Product", productSchema);
